47f49385ffaf4a5285267570a36315946f5d5170
ajitneosoft <ajit.panigrahi@neosoftmail.com>
2022-12-18 10:52:18 +0530

    first commit of app

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT=process.env.NODE_ENV || 3000;
+
+app.get("/",(req,resp)=>{
+    resp.status(200).json({
+        message: "Hello World"
+    });
+});
+
+
+app.post("/",(req,resp)=>{
+    return resp.status(200).json({
+        message: req.body
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is listening on port ",PORT);
+})
