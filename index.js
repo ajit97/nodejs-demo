3ffbfdbd795b33daea3fb495e6c9036a3f6e6ab5
Ajit Panigrahi <panigrahiajit97@gmail.com>
2023-02-01 12:48:52 +0530

    posts added

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.use(express.urlencoded({extended:true}));
 
 const PORT=process.env.NODE_ENV || 3000;
 
+const posts = [
+    {
+        id:1,
+        title:"First Post"
+    }
+]
+
+
 app.get("/",(req,resp)=>{
     resp.status(200).json({
         message: "Hello World"
@@ -17,7 +25,9 @@ app.post("/",(req,resp)=>{
     return resp.status(200).json({
         message: req.body
     })
-})
+});
+
+
 
 app.listen(PORT,()=>{
     console.log("Server is listening on port ",PORT);
